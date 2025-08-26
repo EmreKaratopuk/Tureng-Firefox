45ea1bf1b39dbe1c25bd4731b6497573418fcc53
Emre Karatopuk <emkarali04@gmail.com>
2025-08-26 23:21:27 +0200

    style: reformat code

--- Tureng/scripts/background.js
+++ Tureng/scripts/background.js
@@ -1,21 +1,22 @@
 //Create a function to open the new tab
-function newTab(info,tab)
-{
-  const { menuItemId } = info
+function newTab(info, tab) {
+  const { menuItemId } = info;
 
-  if (menuItemId === 'tureng-text-search'){
+  if (menuItemId === "tureng-text-search") {
     browser.tabs.create({
-      url: "https://tureng.com/tr/turkce-ingilizce/" + info.selectionText.trim()
-    })}};
+      url:
+        "https://tureng.com/tr/turkce-ingilizce/" + info.selectionText.trim(),
+    });
+  }
+}
 
 //Create context menu options.
 
 browser.contextMenus.create({
   title: "Tureng: '%s' ",
   id: "tureng-text-search",
-  contexts: ["selection"]
+  contexts: ["selection"],
 });
-
 
 //This tells the context menu what function to run when the option is selected
 
--- Tureng/scripts/popup.js
+++ Tureng/scripts/popup.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 function Word(id, usage, word, type, definition, definitionType) {
   this.id = id;
@@ -10,24 +10,28 @@ function Word(id, usage, word, type, definition, definitionType) {
 }
 
 function notFound(str) {
-  $('#content').html(`
+  $("#content").html(`
     <div class="alert alert-warning" role="alert">
       <strong>Maalesef,</strong> bir sonuç bulamadık, kelimeyi basitleştirmeyi deneyin ya da <a href="https://www.google.com/search?q=${str}" target="_blank" ><i class="fa fa-google" aria-hidden="true"></i>oogle</a>
     </div>
   `);
 
-  document.getElementById('loading').style.display = 'none';
+  document.getElementById("loading").style.display = "none";
 }
 
 function sanitize(str) {
-  document.getElementById('content').innerHTML = '';
+  document.getElementById("content").innerHTML = "";
 
-  document.getElementById('search-input').value = str;
+  document.getElementById("search-input").value = str;
 
-  document.getElementById('loading').style.display = 'block';
+  document.getElementById("loading").style.display = "block";
 
-  document.getElementsByClassName('inner-shadow')[0].style.backgroundColor = '#BD1E2C';
-  $('.pie, .dot span').css('background-color', '#' + ((1 << 24) * Math.random() | 0).toString(16));
+  document.getElementsByClassName("inner-shadow")[0].style.backgroundColor =
+    "#BD1E2C";
+  $(".pie, .dot span").css(
+    "background-color",
+    "#" + (((1 << 24) * Math.random()) | 0).toString(16)
+  );
 
   return str;
 }
@@ -36,17 +40,20 @@ function tureng(str) {
   str = sanitize(str);
 
   $.get({
-    url: 'https://tureng.com/tr/turkce-ingilizce/' + str,
+    url: "https://tureng.com/tr/turkce-ingilizce/" + str,
     complete: function (xhr) {
       if (xhr.status != 200) {
         notFound(str);
       }
     },
     success: (data) => {
-      const checkSearchResults = $(data).find('.searchResultsTable');
+      const checkSearchResults = $(data).find(".searchResultsTable");
 
-      if ($(data).find('ul.suggestion-list li').length > 0 && checkSearchResults.length === 0) {
-        $('#content').html(`
+      if (
+        $(data).find("ul.suggestion-list li").length > 0 &&
+        checkSearchResults.length === 0
+      ) {
+        $("#content").html(`
             <div class="list-group">
               <a href="https://www.google.com/search?q=${str}" target="_blank" class="list-group-item active">
                 Bunlardan biri değilse,  <i class="fa fa-google" aria-hidden="true"></i>oogle'layın!
@@ -54,30 +61,38 @@ function tureng(str) {
             </div>
           `);
 
-        $(data).find('ul.suggestion-list li').each((i, el) => {
-          $('#content .list-group').append(`
-            <a href="#" class="list-group-item list-group-item-action">${safeResponse.cleanDomString(el.textContent.trim())}</a>
+        $(data)
+          .find("ul.suggestion-list li")
+          .each((i, el) => {
+            $("#content .list-group").append(`
+            <a href="#" class="list-group-item list-group-item-action">${safeResponse.cleanDomString(
+              el.textContent.trim()
+            )}</a>
           `);
-        });
+          });
 
-        $('.list-group-item-action').click(function () {
+        $(".list-group-item-action").click(function () {
           tureng($(this).text());
         });
-        document.getElementById('voice-tts').style.display = 'none';
+        document.getElementById("voice-tts").style.display = "none";
       }
 
       if (checkSearchResults.length > 0) {
         $.each(checkSearchResults, function () {
           let translations = [];
-          let eachRow = $(this).find('tr');
+          let eachRow = $(this).find("tr");
 
-          $('#content').append(`
+          $("#content").append(`
             <table class="table table-striped table-hover">
               <thead class="thead-default">
                 <tr>
                   <th>#</th>
-                  <th>${safeResponse.cleanDomString($(eachRow[0]).find('.c2').text())}</th>
-                  <th>${safeResponse.cleanDomString($(eachRow[0]).find('.c3').text())}</th>
+                  <th>${safeResponse.cleanDomString(
+                    $(eachRow[0]).find(".c2").text()
+                  )}</th>
+                  <th>${safeResponse.cleanDomString(
+                    $(eachRow[0]).find(".c3").text()
+                  )}</th>
                   </tr>
               </thead>
               <tbody>
@@ -91,71 +106,119 @@ function tureng(str) {
 
           array.shift();
           array.forEach((el) => {
-            if ($(el).find('td').eq(0).attr('colspan') == 2 && $(el).find('td b').length > 0) {
+            if (
+              $(el).find("td").eq(0).attr("colspan") == 2 &&
+              $(el).find("td b").length > 0
+            ) {
               // subject of the word
             }
 
-            if ($(el).find('td.rc0') != null && $(el).className == null && $(el).attr('style') == null && $(el).find('td b').length == 0) {
-              const $id = $(el).children('td')[0].textContent;
-              const $usage = $(el).children('td').eq(1).text().trim();
-              const $word = $(el).children('td').eq(2).find('a').text().trim();
-              const $type = $(el).children('td').eq(2).find('i').text().trim() != '' ? $(el).children('td').eq(2).find('i').text().trim() : '';
-              const $definition = $(el).children('td').eq(3).find('a').eq(0).text().trim();
-              const $definitionType = $(el).children('td').eq(3).find('i').text().trim() ? $(el).children('td').eq(3).find('i').eq(0).text().trim() : '';
-              const translation = new Word($id, $usage, $word, $type, $definition, $definitionType);
+            if (
+              $(el).find("td.rc0") != null &&
+              $(el).className == null &&
+              $(el).attr("style") == null &&
+              $(el).find("td b").length == 0
+            ) {
+              const $id = $(el).children("td")[0].textContent;
+              const $usage = $(el).children("td").eq(1).text().trim();
+              const $word = $(el).children("td").eq(2).find("a").text().trim();
+              const $type =
+                $(el).children("td").eq(2).find("i").text().trim() != ""
+                  ? $(el).children("td").eq(2).find("i").text().trim()
+                  : "";
+              const $definition = $(el)
+                .children("td")
+                .eq(3)
+                .find("a")
+                .eq(0)
+                .text()
+                .trim();
+              const $definitionType = $(el)
+                .children("td")
+                .eq(3)
+                .find("i")
+                .text()
+                .trim()
+                ? $(el).children("td").eq(3).find("i").eq(0).text().trim()
+                : "";
+              const translation = new Word(
+                $id,
+                $usage,
+                $word,
+                $type,
+                $definition,
+                $definitionType
+              );
               translations.push(translation);
             }
           });
           translations.forEach((e) => {
-            $('#content table tbody:last').append(`
+            $("#content table tbody:last").append(`
           <tr>
-            <th scope="row" class="align-middle"">${safeResponse.cleanDomString(e.usage)}</th>
-            <td><a data-href="${safeResponse.cleanDomString(e.word)}">${safeResponse.cleanDomString(e.word)}</a> ${safeResponse.cleanDomString(e.type) != '' ? '<small>(' + safeResponse.cleanDomString(e.type) + ')</small>' : ''}</td>
-            <td><a data-href="${safeResponse.cleanDomString(e.definition)}">${safeResponse.cleanDomString(e.definition)}</a> ${safeResponse.cleanDomString(e.definitionType) != '' ? '<small>(' + safeResponse.cleanDomString(e.definitionType) + ')</small>' : ''}</td>
+            <th scope="row" class="align-middle"">${safeResponse.cleanDomString(
+              e.usage
+            )}</th>
+            <td><a data-href="${safeResponse.cleanDomString(
+              e.word
+            )}">${safeResponse.cleanDomString(e.word)}</a> ${
+              safeResponse.cleanDomString(e.type) != ""
+                ? "<small>(" + safeResponse.cleanDomString(e.type) + ")</small>"
+                : ""
+            }</td>
+            <td><a data-href="${safeResponse.cleanDomString(
+              e.definition
+            )}">${safeResponse.cleanDomString(e.definition)}</a> ${
+              safeResponse.cleanDomString(e.definitionType) != ""
+                ? "<small>(" +
+                  safeResponse.cleanDomString(e.definitionType) +
+                  ")</small>"
+                : ""
+            }</td>
             </tr>
           `);
-          })
+          });
         });
-        $('table thead').click(function (e) {
+        $("table thead").click(function (e) {
           const getParentTable = $(e.target).parent().parent().parent()[0];
-          $(getParentTable).find('tbody').first().fadeToggle('fast')
+          $(getParentTable).find("tbody").first().fadeToggle("fast");
         });
 
-        $('table tbody a').click(function (e) {
-          tureng($(this).data('href'));
+        $("table tbody a").click(function (e) {
+          tureng($(this).data("href"));
         });
-        document.getElementById('voice-tts').style.display = 'block';
+        document.getElementById("voice-tts").style.display = "block";
       }
-    }
+    },
   }).done(() => {
-    document.getElementById('loading').style.display = 'none';
+    document.getElementById("loading").style.display = "none";
   });
 }
 
-
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   const input_field = document.getElementById("search-input");
   if (input_field) {
     setTimeout(() => {
       input_field.focus();
       input_field.select();
-}, 50);  };
+    }, 50);
+  }
 
-  document.getElementById('tureng').addEventListener('click', () => {
-    tureng(document.getElementById('search-input').value);
+  document.getElementById("tureng").addEventListener("click", () => {
+    tureng(document.getElementById("search-input").value);
   });
 
   $('[data-toggle="tooltip"]').tooltip();
-
 });
 
-document.getElementById('search-input').addEventListener('keypress', function (e) {
-  if (e.key === 'Enter') {
-    tureng(document.getElementById('search-input').value);
-  }
-});
+document
+  .getElementById("search-input")
+  .addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+      tureng(document.getElementById("search-input").value);
+    }
+  });
 
-// Popup açılışında seçili kelimeyi search_box'a yapıştırır, formu submit eder. 
+// Popup açılışında seçili kelimeyi search_box'a yapıştırır, formu submit eder.
 // Paste the selected word to the search_box on popup open, then submit the form.
 window.onload = async () => {
   const [tab] = await browser.tabs.query({ active: true, currentWindow: true });
@@ -169,11 +232,10 @@ window.onload = async () => {
     return; // ignoring an unsupported page like about:addons
   }
 
-  document.getElementById('search-input').value = result;
+  document.getElementById("search-input").value = result;
   if (result.length > 0) {
     document.getElementById("tureng").click();
   }
-
 };
 
 // TTS
@@ -186,31 +248,33 @@ const playPronunciation = async (text, lang) => {
   audio.load();
 
   await browser.permissions.request({
-    origins: [
-      "https://translate.google.com/*",
-    ]
+    origins: ["https://translate.google.com/*"],
   });
 
-  await audio.play().catch(e => log.error(logDir, "playAudio()", e, url));
+  await audio.play().catch((e) => log.error(logDir, "playAudio()", e, url));
 };
 
-document.getElementById('flag-tr').addEventListener('click', () => {
-  playPronunciation(document.getElementById('search-input').value, 'tr');
+document.getElementById("flag-tr").addEventListener("click", () => {
+  playPronunciation(document.getElementById("search-input").value, "tr");
 });
 
-document.getElementById('flag-us').addEventListener('click', () => {
-  playPronunciation(document.getElementById('search-input').value, 'en-us');
+document.getElementById("flag-us").addEventListener("click", () => {
+  playPronunciation(document.getElementById("search-input").value, "en-us");
 });
 
-document.getElementById('flag-uk').addEventListener('click', () => {
-  playPronunciation(document.getElementById('search-input').value, 'en-gb');
+document.getElementById("flag-uk").addEventListener("click", () => {
+  playPronunciation(document.getElementById("search-input").value, "en-gb");
 });
 
-document.getElementById('flag-au').addEventListener('click', () => {
-  playPronunciation(document.getElementById('search-input').value, 'en-au');
+document.getElementById("flag-au").addEventListener("click", () => {
+  playPronunciation(document.getElementById("search-input").value, "en-au");
 });
 
 // tureng-logo
-document.getElementById('tureng-logo').addEventListener('click', () => {
-  chrome.tabs.create({ url: 'http://tureng.com/tr/turkce-ingilizce/' + document.getElementById('search-input').value });
+document.getElementById("tureng-logo").addEventListener("click", () => {
+  chrome.tabs.create({
+    url:
+      "http://tureng.com/tr/turkce-ingilizce/" +
+      document.getElementById("search-input").value,
+  });
 });
